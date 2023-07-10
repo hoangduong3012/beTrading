056a1ea244485d287d3b2c73950a47779fab89c0
hoangbuiifv <hoang-bq@if-vietnam.com>
2023-07-10 20:28:07 +0700

    fix confflict

---
+++ src/api/symbol/controllers/symbol.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * symbol controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::symbol.symbol');
